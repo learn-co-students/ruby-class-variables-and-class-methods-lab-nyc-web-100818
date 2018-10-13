93aa780dab04753d6c54c5bd908a44f081523484
github username <github email address>
2018-10-13 14:37:05 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -3,56 +3,55 @@ class Song
 attr_accessor :name, :artist, :genre
 
 @@count = 0
-@@artists = []
 @@genres = []
-
-
+@@artists = []
 
 def initialize(name, artist, genre)
   @name = name
   @artist = artist
   @genre = genre
-   @@count += 1
-   @@name = name
-   @@artists<<artist
-   @@genres<<genre
- end
-
+  @@count += 1
+  @@genres << genre
+  @@artists << artist
+end
 
 def self.count
   @@count
-end
-
-def self.artists
-  @@artists.uniq
 end
 
 def self.genres
   @@genres.uniq
 end
 
+def self.artists
+  @@artists.uniq
+end
+
+
 def self.genre_count
   genre_count = {}
-  @@genres.each do |genre|
-  if genre_count[genre]
-    genre_count[genre] +=1
-  else
-    genre_count[genre] = 1
+  @@genres.each do |genres|
+    if genre_count[genres]  #if genre_count has the genres being itereated
+      genre_count[genres] += 1 #genre_count [genre] will be incremented in number
+    else
+      genre_count[genres] = 1
+    end
   end
+  genre_count
 end
-genre_count
-end
+
 
 def self.artist_count
   artist_count = {}
-  @@artists.each do |artist|
-  if artist_count[artist]
-    artist_count[artist] +=1
-  else
-    artist_count[artist] = 1
+  @@artists.each do |artists|
+    if artist_count[artists]  #if artist_count has the artists being itereated
+      artist_count[artists] += 1 #artist_count [artist] will be incremented in number
+    else
+      artist_count[artists] = 1
+    end
   end
-end
-artist_count
+  artist_count
 end
 
-end
+
+end #end of Song class
